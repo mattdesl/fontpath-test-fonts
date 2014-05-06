cb08011a8ae5cd33411002998debc21abaa4baf0
mattdesl <dave.des@gmail.com>
2014-05-05 21:49:01 -0400

    first commit

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#build all TTF files using 64pt and CommonJS output
+mkdir -p lib
+
+SAVEIFS=$IFS
+#why is bash so bloody cryptic
+IFS=$(echo -en "\n\b")
+
+for i in `find input -type f | egrep '\.(ttf|otf|woff)$'`; 
+do
+	s=$i;
+	s=${s##*/};
+	echo -n "$s: "; 
+	# s=${s%.*};
+	fontpath "$i" -o "lib/$s.js" -s 64 --commonJS; 
+done
+
+IFS=$SAVEIFS
---
+++ index.js
@@ -0,0 +1,19 @@
+//If you're packaging with browserify, you'll need to use 'fontpath-test-fonts/lib/***.ttf' etc
+var fs = require('fs');
+var path = require('path');
+var slug = require('slug');
+
+var files = fs.readdirSync('lib');
+
+files = files.filter(function(f) {
+	return path.extname(f) === '.js';
+});
+
+module.exports = {};
+
+files.forEach(function(f) {
+	var key = slug(path.basename(f, '.js'));
+	if (!key)
+		return;
+	module.exports[key] = require( '.' + path.sep + path.join('lib', f) );
+});
